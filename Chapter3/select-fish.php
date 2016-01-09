17938195cc37724af63d08a93c6e2368a737a01d
na3ksg <superkasago@yahoo.co.jp>
2016-01-09 17:30:50 +0900

    Revert "削除"
    This reverts commit 248a5dbb67c7f65e4ebfd06f073ecdac3a72217e.

---
+++ Chapter3/select-fish.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+//--------------------------------------------------------------------
+// 商品一覧の定義
+$fish = array( "サンマ","マグロ","カサゴ","クエ","ヒラメ","フナ","サバ");
+//--------------------------------------------------------------------
+// パラメータに応じて処理を変える
+if (isset($_GET["fish"])) {
+    show_item();
+} else {
+    show_form();
+}
+//--------------------------------------------------------------------
+// 選択したアイテムを表示する
+function show_item() {
+    $fish = $_GET["fish"];
+    $fish_html = htmlspecialchars($fish); // HTML変換
+    echo "魚「{$fish_html}」をゲットだぜ！";
+}
+// フォームを表示する
+function show_form() {
+    global $fish; // グローバル宣言
+    // 選択肢の文字列を生成する
+    $options = "";
+    foreach ($fish as $item) {
+        $options .= "<option value='$item'>$item</option>";
+    }
+    // フォームをヒアドキュメントで表示
+    echo <<< __FORM__
+<form>
+<select name="fish">
+    <option>魚を選択</option>
+    {$options}
+</select>
+<input type="submit" value="購入" />
+</form>
+__FORM__;
+}
+?>
+    </body>
+</html>
