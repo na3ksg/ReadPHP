b396de68e39083ac98c9658a63ab68cd74059d0b
na3ksg <superkasago@yahoo.co.jp>
2016-01-09 15:42:27 +0900

    P134 サンプル　置換済

--- Chapter3/select-fish.php
+++ Chapter3/select-fish.php
@@ -10,43 +10,44 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        //-------------
-        //商品一覧の定義
-        $fish = array(
-           "サンマ","マグロ","カサゴ","クエ","ヒラメ","フナ","サバ");
-        //-------------
-        //パラメーターに応じて処理を変える
-        if (isset($_GET["fish"])){
-            show_item();
-        } else {
-            show_form();
-        }
-        //--------------
-        //選択したアイテムを表示する
-        function show_item(){
-            $fish = $_GET["fish"];
-            $fish_html = htmlspecialchars($fish);//HTML変換
-            echo "商品{$fish_html}をゲットだぜ！";
-        }
-        //フォームを表示する
-        function show_form(){
-            global $fish;//グローバル宣言
-            //選択肢の文字列を生成する
-            $options = "";
-            foreach ($fish as $item){
-                $options.= "<option value = '$item'>$item</option>";
-            }
-            //フォームをヒアドキュメントで表示
-            echo <<<_FORM_
-            <form>
-            <select name=fish">
-            <option>魚を選択</option>
-            {$options}
-            </select>
-            <input type="submit" value = "購入"/>
-            </form>
-_FORM_;
-        }
-        ?>
+//--------------------------------------------------------------------
+// 商品一覧の定義
+$fish = array( "目薬","日焼け止め","シャンプー","虫除けスプレー",
+                "石けん","ガム","チョコレート","バナナ");
+//--------------------------------------------------------------------
+// パラメータに応じて処理を変える
+if (isset($_GET["fish"])) {
+    show_item();
+} else {
+    show_form();
+}
+//--------------------------------------------------------------------
+// 選択したアイテムを表示する
+function show_item() {
+    $fish = $_GET["fish"];
+    $fish_html = htmlspecialchars($fish); // HTML変換
+    echo "商品「{$fish_html}」を購入しました!!";
+}
+// フォームを表示する
+function show_form() {
+    global $fish; // グローバル宣言
+    // 選択肢の文字列を生成する
+    $options = "";
+    foreach ($fish as $item) {
+        $options .= "<option value='$item'>$item</option>";
+    }
+    // フォームをヒアドキュメントで表示
+    echo <<< __FORM__
+<form>
+<select name="fish">
+    <option>商品を選択</option>
+    {$options}
+</select>
+<input type="submit" value="購入" />
+</form>
+__FORM__;
+}
+//--------------------------------------------------------------------
+?>
     </body>
 </html>
